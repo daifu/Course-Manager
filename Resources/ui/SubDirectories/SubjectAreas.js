0af48770320204439941aacb87f8c6fe3307da04
daifu ye <daifu.ye@gmail.com>
2012-04-02 00:23:23 -0700

    Automatically update and insert the content to the database if there is blank in the database.

--- Resources/ui/SubDirectories/SubjectAreas.js
+++ Resources/ui/SubDirectories/SubjectAreas.js
@@ -1,17 +1,35 @@
+require('lib/require_patch').monkeypatch(this);
+
 // Create a table view for all the subject areas
 (function(){
-    
+   
     // Working with the current window
     var table = require('lib/tables'),
         subject_area_table,
         db = require('model/db'),
+        httpReq = require('lib/http_requests'),
         dbData,
         term = Ti.UI.currentWindow.dataToPass.term;
+        
+    var callback = function (retData) {
+        var dbData = db.updateAndGetSubjectAreas(retData);
+        subject_area_table = new table.createPullToRefreshView(dbData, "../../images/whiteArrow.png", "subject_areas");
+
+        //Add table view to the instance
+        Ti.UI.currentWindow.add(subject_area_table);
+    };
    
     dbData = db.getSubjectAreas(term);
-    subject_area_table = new table.createPullToRefreshView(dbData, "../../images/whiteArrow.png", "subject_areas");
+    
+    if (dbData.length === 0) {
+        httpReq.httpGetSubjectAreas(callback, term);
+    } else {
+        subject_area_table = new table.createPullToRefreshView(dbData, "../../images/whiteArrow.png", "subject_areas");
 
-    //Add table view to the instance
-    Ti.UI.currentWindow.add(subject_area_table);
+        //Add table view to the instance
+        Ti.UI.currentWindow.add(subject_area_table);
+
+    }
+    
     
 })();
--- Resources/ui/SubDirectories/Subjects.js
+++ Resources/ui/SubDirectories/Subjects.js
@@ -1,3 +1,4 @@
+require('lib/require_patch').monkeypatch(this);
 // Create a table view for all the subject areas
 (function(){
     
@@ -7,15 +8,31 @@
         subjects_table,
         dbData,
         db = require('model/db'),
+        httpReq = require('lib/http_requests'),
         term = Ti.UI.currentWindow.dataToPass.term,
         subject = Ti.UI.currentWindow.dataToPass.subject;
 
     instance = Ti.UI.currentWindow;
     dbData = db.getSubjects(term, subject);
-    // Ti.API.info(JSON.stringify(dbData));
-    subjects_table = table.createPullToRefreshView(dbData, "../../images/whiteArrow.png", "subjects");
+    
 
-    //Add table view to the instance
-    instance.add(subjects_table);
+    var callback = function(retData) {
+        var dbData = db.updateAndGetSubjects(retData);
+        subjects_table = new table.createPullToRefreshView(dbData, "../../images/whiteArrow.png", "subjects");
 
+        //Add table view to the instance
+        Ti.UI.currentWindow.add(subjects_table);
+    };
+    
+    dbData = db.getSubjects(term, subject);
+
+    if(dbData.length === 0) {
+        httpReq.httpGetSubjects(callback, term, subject);
+    } else {
+        subjects_table = new table.createPullToRefreshView(dbData, "../../images/whiteArrow.png", "subjects");
+
+        //Add table view to the instance
+        Ti.UI.currentWindow.add(subjects_table);
+    }
+    
 })();
